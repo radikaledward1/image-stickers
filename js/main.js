8f6e8f65be1fbd39bc585e6ef6e444d5df661728
Oscar Adrian Gonzalez Gamboa <gonzalez.gamboa1@gmail.com>
2017-10-16 13:46:44 -0500

    validations to restrict just 2 elements inside the canvas

--- js/main.js
+++ js/main.js
@@ -1,86 +1,85 @@
-$(document).ready(function() {
+var elements_counter = 0;
 
-  $( init );
+// Draggable elements
 
-  function init() {
+$('.opt-sticker').draggable( {
+  //containment: '#canvas',
+  cursor: 'move',
+  //snap: '#canvas',
+  revert: "invalid",
+  helper: 'clone',
+  opacity: 0.35
+} );
 
-    // Draggable elements
+$('.opt-strap').draggable( {
+  //containment: '#canvas',
+  cursor: 'move',
+  //snap: '#canvas',
+  revert: "invalid",
+  helper: 'clone',
+  opacity: 0.35
+} );
 
-    $('.opt-sticker').draggable( {
-      //containment: '#canvas',
-      cursor: 'move',
-      //snap: '#canvas',
-      revert: "invalid",
-      helper: 'clone',
-      opacity: 0.35
-    } );
+$("#procesar").click(function(){
 
-    $('.opt-strap').draggable( {
-      //containment: '#canvas',
-      cursor: 'move',
-      //snap: '#canvas',
-      revert: "invalid",
-      helper: 'clone',
-      opacity: 0.35
-    } );
+    var element = $("#canvas");
+    //var getCanvas;
 
-  }
+    /*html2canvas(element, {
+     onrendered: function (image) {
 
-  $("#procesar").click(function(){
+            getCanvas = image;
 
-      var element = $("#canvas");
-      //var getCanvas;
+            var imgageData = getCanvas.toDataURL("image/png");
+    
+            var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+            $("#descargar").attr("download", "pruebas-descarga.png").attr("href", newData);
 
-      /*html2canvas(element, {
-       onrendered: function (image) {
+         }
+     });*/
 
-              getCanvas = image;
+    html2canvas(element, {
+         onrendered: function (image) {
 
-              var imgageData = getCanvas.toDataURL("image/png");
-      
-              var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-              $("#descargar").attr("download", "pruebas-descarga.png").attr("href", newData);
+            //get the rendered canvas, and create, another one with the new size
+            var extra_canvas = document.createElement("canvas");
+            extra_canvas.setAttribute('width',736);
+            extra_canvas.setAttribute('height',796);
+            var ctx = extra_canvas.getContext('2d');
+            ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, 736, 796);
 
-           }
-       });*/
+            /*var imgageData = extra_canvas.toDataURL("image/png");
 
-      html2canvas(element, {
-           onrendered: function (image) {
+            var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+            $("#descargar").attr("download", "pruebas-descarga.png").attr("href", newData);*/
 
-              //get the rendered canvas, and create, another one with the new size
-              var extra_canvas = document.createElement("canvas");
-              extra_canvas.setAttribute('width',736);
-              extra_canvas.setAttribute('height',796);
-              var ctx = extra_canvas.getContext('2d');
-              ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, 736, 796);
+            //use FileSaver to download the image canvas correctly
+            extra_canvas.toBlob(function(blob) {
 
-              /*var imgageData = extra_canvas.toDataURL("image/png");
+                saveAs(blob, "image-sticker.png");
+            });
+        }
+    });
 
-              var newData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
-              $("#descargar").attr("download", "pruebas-descarga.png").attr("href", newData);*/
-
-              //use FileSaver to download the image canvas correctly
-              extra_canvas.toBlob(function(blob) {
-
-                  saveAs(blob, "image-sticker.png");
-              });
-          }
-      });
-
-  });
+});
 
 
-  $('#limpiar').click(function() {
+$('#limpiar').click(function() {
 
-    clear_canvas();
-
-  });
+  clear_canvas();
 
 });
 
 function clear_canvas() {
 
+  //Remove all elements in the canvas
   $('#canvas').find('.opt-sticker, .opt-strap').remove();
+
+  //Restart the elements_counter
+  elements_counter = 0;
+
+  //The canvas is droppable again
+  $('#canvas').droppable('enable');
 
 }
 
@@ -116,9 +115,6 @@ function readURL(input) {
 
 function handleDropEvent( event, ui ) {
 
-  //implementar que no se pueden poner mas de un mismo elemento en el canvas. (Listo)
-  //implementar que no se puedan poner mas de dos elementos en el canvas.
-
   var object = ui.draggable.clone();
   var object_class = '.' + object.attr('class').split(' ')[1];
 
@@ -126,6 +122,12 @@ function handleDropEvent( event, ui ) {
 
     object.draggable({containment: '#canvas', snap: '#canvas'});
     $(this).append(object);
+
+    elements_counter++;
+
+    if (elements_counter == 2) {
+      $(this).droppable('disable');
+    }
 
   }
 
